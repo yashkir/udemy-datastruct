ca09a144953f2f56ae79340826c1872348f144d2
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2020-12-16 14:40:54 -0500

    recursion

---
+++ recursion.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// Print on Ascent
+void f1(int n) {
+    if (n > 0) {
+        printf("%d ", n);
+        f1(n - 1);
+    }
+}
+
+// Print on Return/Descent
+void f2(int n) {
+    if (n > 0) {
+        f2(n - 1);
+        printf("%d ", n);
+    }
+}
+
+int main() {
+    int x = 10;
+
+    f1(x);
+    printf("\n");
+    f2(x);
+
+    return 0;
+}
