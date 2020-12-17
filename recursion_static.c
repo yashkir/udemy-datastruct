e15bee8d0a1337052f271ed11c87c9ca71ee7332
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2020-12-17 13:25:26 -0500

    recursion_static

---
+++ recursion_static.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int f1(int n) {
+    static int x = 0;
+
+    if (n > 0) {
+        x++;
+        return f1(n - 1) + x;
+    }
+    return 0;
+}
+
+int main() {
+    int a = 5;
+    printf("%d", f1(a));
+
+    return 0;
+}
